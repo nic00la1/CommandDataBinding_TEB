f23e4f396f0dd177d9a31549b2c585980f5d18d8
nic00la1 <nic00la.kaleta@gmail.com>
2025-03-06 13:14:20 +0100

    styl

--- CommandDataBinding_TEB/MainViewModel.cs
+++ CommandDataBinding_TEB/MainViewModel.cs
@@ -14,9 +14,9 @@ namespace CommandDataBinding_TEB
             // Inicjalizacja listy osób
             People = new ObservableCollection<Person>
                 {
-                    new Person { Name = "Jan Kowalski", ImageUrl = "person1.png", Description = "Opis Jana Kowalskiego" },
-                    new Person { Name = "Anna Nowak", ImageUrl = "person3.webp", Description = "Opis Anny Nowak" },
-                    new Person { Name = "Piotr Wiœniewski", ImageUrl = "person2.png", Description = "Opis Piotra Wiœniewskiego" }
+                    new Person { Name = "Jan Kowalski", ImageUrl = "person1.png", Age=18,Description = "Mam na imiê Janek, uczê siê w Technikum Teb Edukacja £ódŸ.", Gender="Mê¿czyzna", Hobby="Jazda na rowerze, Rysowanie, Siatkówka" },
+                    new Person { Name = "Anna Nowak", ImageUrl = "person3.webp",Age=19 ,Description = "Opis Ani Nowak", Gender="Kobieta", Hobby="Programowanie, Gotowanie, Czytanie Biblii" },
+                    new Person { Name = "Piotr Wiœniewski", ImageUrl = "person2.png", Age=17, Description = "Opis Piotra Wiœniewskiego", Gender="Mê¿czyzna", Hobby="Gra na gitarze, Œpiewanie, Jogging" }
                 };
 
             // Inicjalizacja komendy
@@ -42,7 +42,10 @@ namespace CommandDataBinding_TEB
     public class Person
     {
         public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        public string Hobby { get; set; }
     }
 }
